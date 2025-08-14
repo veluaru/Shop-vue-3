dc76facb36aec6cb92cb52f1447ca5e6e9283949
unknown <angie.castaneda@guane.com.co>
2025-08-14 15:23:04 -0500

    :rocket: Vue router config for deploy

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import ProductDetailsView from '@/views/ProductDetailsView.vue'
 import SearchView from '@/views/SearchView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
