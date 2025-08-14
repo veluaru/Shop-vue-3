058b616bb27b6670e55c594ec6f8743170bfda4a
unknown <angie.castaneda@guane.com.co>
2025-08-14 15:10:59 -0500

    :rocket: Vite config for deploy

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: 'https://veluaru.github.io/Shop-vue-3/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
