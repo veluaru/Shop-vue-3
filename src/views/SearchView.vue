432723e132e5c86d2b4f4bdfda2ea8ed84896617
Angie Castañeda <angie.castaneda@guane.com.co>
2025-09-29 19:43:17 -0500

    :sparkles: Adding category filters in search view

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="search-view">
     <div class="search-view__header" v-if="!loading">
-      <h1 v-if="!showAllProducts" class="search-view__header__title">Search results for "{{ route.query.text }}"</h1>
+      <h1 v-if="!showAllProducts" class="search-view__header__title">
+        Search results for "{{ route.query.text }}"
+      </h1>
       <h1 v-if="showAllProducts" class="search-view__header__title">All products</h1>
       <span class="search-view__header__sub-title">
         {{ filteredProducts.length }}&nbsp;products found
       </span>
+    </div>
+    <div class="search-view__filters" v-if="!loading">
+      <button
+        v-for="category in filterCategories"
+        :key="category"
+        class="search-view__filters__button"
+        :class="{ 'search-view__filters__button--active': selectedCategory === category }"
+        @click="changeCategory(category)"
+      >
+        {{ category }}
+      </button>
     </div>
     <div class="search-view__products" v-if="!loading">
       <ProductCard
@@ -35,34 +48,73 @@ const route = useRoute()
 const router = useRouter()
 const productsStore = useProductsStore()
 const { allProducts, searchText } = storeToRefs(productsStore)
-const filteredProducts = ref([]);
+const filteredProducts = ref([])
 const loading = ref(false)
-const showAllProducts = ref(true);
+const showAllProducts = ref(true)
+const selectedCategory = ref('All')
+const filterCategories = ['All', ...categories]
+
+const applyFilters = () => {
+  let products = allProducts.value
+  const text = searchText.value
+
+  if (text && text !== '') {
+    products = products.filter((product) =>
+      product.title.toLowerCase().includes(text.toLowerCase())
+    )
+  }
+
+  if (selectedCategory.value !== 'All') {
+    products = products.filter(
+      (product) => product.category.toLowerCase() === selectedCategory.value.toLowerCase()
+    )
+  }
+
+  filteredProducts.value = products
+}
+
+const changeCategory = (category) => {
+  selectedCategory.value = category
+  applyFilters()
+}
 
 const changeView = (viewName, params) => {
   const parameters = params ? { params: { id: params.id } } : {}
   router.push({ name: viewName, ...parameters })
 }
 
-const getProducts = async (text) => {
+const getProducts = async () => {
   loading.value = true
-  //   await productsStore.getProductsByValue(route.query.text || '')
-  await productsStore.getAllProducts();
-  filteredProducts.value = text && text !== '' ? allProducts.value.filter(product => product.title.toLowerCase().includes(text.toLowerCase())) : allProducts.value;
-  showAllProducts.value = text && text !== '' ? false : true;
+  await productsStore.getAllProducts()
+  selectedCategory.value = 'All'
+  applyFilters()
+  showAllProducts.value = !searchText.value || searchText.value === ''
   loading.value = false
 }
 
 onBeforeMount(async () => {
-  getProducts(route.query.text);
+  getProducts()
 })
 
 watch(
   () => searchText.value,
-  (newValue) => {
-    getProducts(newValue);
+  (newValue, oldValue) => {
+    if (!newValue && oldValue) {
+      // When search is cleared
+      selectedCategory.value = 'All'
+    }
+    getProducts()
   }
-);
+)
+
+watch(
+  () => route.query.text,
+  (newValue) => {
+    if (newValue !== searchText.value) {
+      searchText.value = newValue
+    }
+  }
+)
 </script>
 
 <style scoped lang="scss">
@@ -70,7 +122,7 @@ watch(
   display: flex;
   flex-direction: column;
   align-items: center;
-  row-gap: 80px;
+  row-gap: 40px;
   padding: 30px 10%;
   &__products {
     display: grid;
@@ -102,6 +154,31 @@ watch(
     &__sub-title {
       font-size: 16px;
       color: #4b5563;
+    }
+  }
+  &__filters {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+    justify-content: flex-start;
+    width: 100%;
+    &:hover {
+      color: white;
+    }
+    &__button {
+      @include main-button;
+      background-color: transparent;
+      border: 1px solid black;
+      color: black;
+      &--active {
+        border: 1px solid transparent;
+        background-color: black;
+        color: white;
+      }
+      &:hover {
+        border: 1px solid transparent;
+        color: white;
+      }
     }
   }
 }
